c8f5bc2f2f665d9ff3cd65014fa0f88b65928c76
Safin-Ali <safin.ali.7205@gmail.com>
2023-10-16 22:43:13 +0600

    module-type-detector utility throw error

--- src/utilities/module-type-detector.ts
+++ src/utilities/module-type-detector.ts
@@ -1,28 +1,35 @@
 import { ModuleLiteral } from '../types/types';
+import logger from './color-logger';
+import { thorwError } from './common-utilities';
 
 
 /**
  * Checks the module type based on the provided module literal and node.
  * @param {ModuleLiteral} module - The module literal (e.g., 'ESM', 'CESM', 'CommonJS').
  * @param {Object|any} node - The node to be checked.
- * @returns {boolean} Returns true if the node matches the specified module type, otherwise false.
+ * @returns {boolean | Error} Returns true if the node matches the specified module type, otherwise false.
  */
-const checkModuleType = (module: ModuleLiteral, node: any):boolean => {
-	if (module === 'ESM') {
-		return node.type === 'ImportDeclaration';
-	} else if (module === 'CESM' || module === 'CommonJS') {
-		const isVariableDeclaration =
-			node.type === 'VariableDeclaration' &&
-			node.declarations[0]?.init?.type === 'CallExpression';
+const checkModuleType = (module: ModuleLiteral, node: any):boolean | any => {
+	try{
+		if (module === 'ESM') {
+			return node.type === 'ImportDeclaration';
+		} else if (module === 'CESM' || module === 'CommonJS') {
+			const isVariableDeclaration =
+				node.type === 'VariableDeclaration' &&
+				node.declarations[0]?.init?.type === 'CallExpression';
 
-		if (module === 'CESM') {
-			return isVariableDeclaration && node.declarations[0].init.callee.name === '__importDefault';
-		} else if (module === 'CommonJS') {
-			return isVariableDeclaration && node.declarations[0].init.callee.name === 'require';
+			if (module === 'CESM') {
+				return isVariableDeclaration && node.declarations[0].init.callee.name === '__importDefault';
+			} else if (module === 'CommonJS') {
+				return isVariableDeclaration && node.declarations[0].init.callee.name === 'require';
+			}
 		}
-	}
 
-	return false; // Handle other cases or invalid module types
+		return false; // Handle other cases or invalid module types
+	} catch (err:any) {
+		logger.error((err.message))
+		thorwError(`Module Type Detecting Failed`);
+	}
 };
 
 export default checkModuleType;
